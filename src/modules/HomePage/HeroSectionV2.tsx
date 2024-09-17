974eb18720a4183b728de922ef72ed4bb342d6c1
auncon <shazzadauncon@gmail.com>
2024-09-17 12:49:06 -0700

    qdrfgefdgh

--- src/modules/HomePage/HeroSectionV2.tsx
+++ src/modules/HomePage/HeroSectionV2.tsx
@@ -1,72 +1,41 @@
-import BoxReveal from "@/components/magicui/box-reveal";
 import ShimmerButton from "@/components/magicui/shimmer-button";
 
-import Image from "next/image";
 import React from "react";
 import NavbarV2 from "../Nav/NavbarV2";
 import { useTheme } from "next-themes";
 import LetterPullup from "@/components/magicui/letter-pullup";
 import BlurIn from "@/components/magicui/blur-in";
 import { NeonGradientCard } from "@/components/magicui/neon-gradient-card";
+import GradualSpacing from "@/components/magicui/gradual-spacing";
 
 const HeroSectionV2 = () => {
   const { theme, setTheme } = useTheme();
   console.log("theme", theme);
   return (
-    <div className="bg-transparent dark:bg-gradient-to-br from-gray-800 via-black to-red-950 text-white w-full h-[800px] overflow-hidden relative">
+    <div className="bg-transparent dark:bg-gradient-to-br from-gray-800 via-black to-purple-950 text-white w-full h-[800px] overflow-hidden relative">
       <NavbarV2 />
       <div className="flex items-center justify-around gap-12 h-full px-[150px]">
         <div className="uppercase ">
           <LetterPullup words="Hi, I am Auncon" />
-          <BlurIn
-            word="Software Developer"
-            className="text-yellow-500 grid text-4xl md:text-9xl font-bold leading-tight mt-6"
-          />
-
-          {/* <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-            <div className="grid text-4xl md:text-9xl font-bold leading-tight">
-              <p className={theme === "light" ?"text-black" :""}>Hi, I am Auncon</p>
-             
-              <span className="text-yellow-500">Software Developer</span>
+          <div className="flex justify-start">
+            <div>
+              <GradualSpacing
+                className="font-display text-start text-4xl font-bold mt-11 -ml-4 tracking-[-0.1em] text-purple-600  md:text-9xl md:leading-[5rem]"
+                text="Software"
+              />
+              <GradualSpacing
+                className="font-display text-left text-4xl font-bold tracking-[-0.1em] text-purple-700  md:text-9xl"
+                text="Developer"
+              />
             </div>
-          </BoxReveal> */}
-          <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-            <h2 className="mt-[.5rem] text-[1rem]">
-              UI library for
-              <span className="text-[#5046e6]">Design Engineers</span>
-            </h2>
-          </BoxReveal>
-          <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-            <div className="mt-[1.5rem]">
-              <p>
-                -&gt; 20+ free and open-source animated components built with
-                <span className="font-semibold text-[#5046e6]"> React</span>,
-                <span className="font-semibold text-[#5046e6]">
-                  {" "}
-                  Typescript
-                </span>
-                ,
-                <span className="font-semibold text-[#5046e6]">
-                  {" "}
-                  Tailwind CSS
-                </span>
-                , and
-                <span className="font-semibold text-[#5046e6]">
-                  {" "}
-                  Framer Motion
-                </span>
-                . <br />
-                -&gt; 100% open-source, and customizable. <br />
-              </p>
-            </div>
-          </BoxReveal>
+          </div>
           <ShimmerButton className="shadow-2xl">
             <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
               View Portfolio
             </span>
           </ShimmerButton>
         </div>
-        <NeonGradientCard className=" w-[750px] h-[600px] items-center justify-center text-center ">
+        <NeonGradientCard className="w-[750px] h-[600px] items-center justify-center text-center ">
           {/* <span className="pointer-events-none z-10 h-full whitespace-pre-wrap bg-gradient-to-br from-[#ff2975] from-35% to-[#00FFF1] bg-clip-text text-center text-6xl font-bold leading-none tracking-tighter text-transparent dark:drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)]">
             Neon Gradient Card
           </span> */}
